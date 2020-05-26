b24d8fd1f9197d13818d9e679b66e42f1188dd13
Ivan Viveros <ivan.viveros@correounivalle.edu.co>
2020-05-26 10:35:41 -0500

    agregados test a usuarios

--- ms_usuarios/apps/usuarios/tests.py
+++ ms_usuarios/apps/usuarios/tests.py
@@ -8,6 +8,10 @@ class UserTestCase(TestCase):
     def setUp(self):
         Usuario.crear_usuario_inicial()
 
-    def test_initial_user(self):
+    def test_initial_user_is_superuser(self):
         user = Usuario.objects.get(username='admin')
         self.assertEqual(user.is_superuser,True)
+
+    def test_initial_user_is_administrator(self):
+        user = Usuario.objects.get(username='admin')
+        self.assertEqual(user.rol,"Administrador")
