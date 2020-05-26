05ff0a30a9b06dcaef84426aa0d3883ddc3776be
Ivan Viveros <ivan.viveros@correounivalle.edu.co>
2020-05-26 10:10:14 -0500

    agregado travis.yml y test a app usuarios

--- ms_usuarios/apps/usuarios/tests.py
+++ ms_usuarios/apps/usuarios/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from apps.usuarios.models import Usuario
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        Usuario.crear_usuario_inicial()
+
+    def test_initial_user(self):
+        user = Usuario.objects.get(username='admin')
+        self.assertEqual(user.is_superuser,True)
